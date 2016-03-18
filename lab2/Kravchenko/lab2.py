c6313e600204c8c26bcc4fa6a9d31db6f8e4c6c0
Alexandr Kravchenko <sasha121196@mail.ru>
2016-03-18 15:09:00 +0400

    Update lab2.py

--- lab2/Kravchenko/lab2.py
+++ lab2/Kravchenko/lab2.py
@@ -3,32 +3,56 @@ import numpy as np
 
 def linear_regression(x, y):
     m = x.shape[0]
-    matrix = np.array(m, 2)
+    matrix = np.ndarray(shape=(m, 2))
     for j in range(m):
         matrix[j, 0] = 1
-    matrix[:, 1] = x
-    return my_function(matrix, y)
+        matrix[j, 1] = x[j].tolist()[0]
+    b = my_function(matrix, y)
+
+    def f(t):
+        p = b[0] + b[1] * t
+        return p
+
+    return f
 
 
 def polynomial_regression(x, y, k):
     m = x.shape[0]
-    matrix = np.array(m, k + 1)
-    for j in range(k):
-        matrix[:, k] = x ** k
-    return my_function(matrix, y)
+    matrix = np.ndarray(shape=(m, k + 1))
+    for j in range(m):
+        matrix[j, 0] = 1
+        for z in range(1, k + 1):
+            matrix[j, z] = x[j].tolist()[0] ** z
+    b = my_function(matrix, y)
+
+    def f(t):
+        p = 0
+        for i in range(k + 1):
+            p += b[i] * (t ** i)
+        return p
+
+    return f
 
 
 def functional_regression(x, y, functions):
     k = len(functions)
     m = x.shape[0]
-    matrix = np.array(m, k + 1)
+    matrix = np.ndarray(shape=(m, k))
     for j in range(m):
-        matrix[j, 0] = 1
-    for i in range(1, k):
-        matrix[:, k] = functions[k](x)
-    return my_function(matrix, y)
+        for z in range(k):
+            matrix[j, z] = functions[z](x[j].tolist()[0])
+    b = my_function(matrix, y)
+
+    def f(t):
+        p = 0
+        for i in range(k):
+            p += b[i] * functions[i](t)
+        return p
+
+    return f
 
 
 def my_function(x, y):
     b = (x.T * x).I * x.T * y
     return b
+
