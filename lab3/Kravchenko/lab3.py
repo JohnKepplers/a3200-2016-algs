c73b6e4c5905544c18b9f735282bb56838d05afa
Alexandr Kravchenko <sasha121196@mail.ru>
2016-06-24 19:28:16 +0400

    Update lab3.py

--- lab3/Kravchenko/lab3.py
+++ lab3/Kravchenko/lab3.py
@@ -1,92 +1,111 @@
 import math
-
-from sklearn import datasets, neighbors
-
-
-def distance(m, x, y, v):
-    if m == 0:
-        return math.sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2), v
-    elif m == 1:
-        return math.fabs(x[0] - y[0]) + math.fabs(x[1] - y[1]), v
-    elif m == 2:
-        if math.fabs(x[0] - y[0]) > math.fabs(x[1] - y[1]):
-            return math.fabs(x[0] - y[0]), v
-        else:
-            return math.fabs(x[1] - y[1]), v
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
 
 
-def data_processing(k):
-    dict_learning = {}
-    dict_test = {}
-    iris = datasets.load_iris()
-    x = iris.data
-    y = iris.target
-    for i in range(len(x)):
-        xx = [(x[i][2]), x[i][3]]
-        if 40 <= i < 50 or 90 <= i < 100 or 140 <= i < 150:
-            dict_test.update({frozenset(xx): [y[i], None]})
-        else:
-            dict_learning.update({frozenset(xx): y[i]})
-    return kNN(k, dict_learning, dict_test, 50)
+class My_kNN():
+    def __init__(self):
+        self.known_x = []
+        self.known_y = []
+        self.unknown_x = []
+        self.unknown_y = []
+        self.is_weight = False
+        self.k = 11
 
-
-def kNN(k, dict_learning, dict_test, weight):
-    for k_test in dict_test.keys():
-        helping_array = []
-        array_of_distances = []
-        d = {}
-        for k_learn, v_learn in dict_learning.items():
-            dist, v = distance(0, list(k_test), list(k_learn), v_learn)
-            d.update({dist: v})
-            helping_array.append(dist)
-        helping_array.sort()
-        a, b, c = 0, 0, 0
-        for i in range(k):
-            array_of_distances.append(helping_array[i])
-            if weight == 0:
-                if d[array_of_distances[i]] == 0:
-                    a += 1
-                elif d[array_of_distances[i]] == 1:
-                    b += 1
-                else:
-                    c += 1
+    def distance(self, m, x, y):
+        if m == 0:
+            return math.sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2)
+        elif m == 1:
+            return math.fabs(x[0] - y[0]) + math.fabs(x[1] - y[1])
+        elif m == 2:
+            if math.fabs(x[0] - y[0]) > math.fabs(x[1] - y[1]):
+                return math.fabs(x[0] - y[0])
             else:
-                if d[array_of_distances[i]] == 0:
-                    a += weight - i
-                elif d[array_of_distances[i]] == 1:
-                    b += weight - i
+                return math.fabs(x[1] - y[1])
+
+    def data_processing(self):
+        iris = datasets.load_iris()
+        x = iris.data
+        y = iris.target
+        for i in range(len(x)):
+            if 40 <= i < 50 or 90 <= i < 100 or 140 <= i < 150:
+                self.known_x += [[x[i][2], x[i][3]]]
+                self.known_y += [y[i]]
+            else:
+                self.unknown_x += [[x[i][2], x[i][3]]]
+                self.unknown_y += [y[i]]
+        return self.kNN()
+
+    def kNN(self):
+        result = []
+        for i in range(len(self.unknown_x)):
+            array_of_neighbours = []
+            array_of_distances = []
+            array_of_weight = [0, 0, 0]
+            for j in range(len(self.known_y)):
+                array_of_neighbours.append(self.known_y[j])
+                array_of_distances.append(self.distance(0, self.unknown_x[i], self.known_x[j]))
+            while len(array_of_neighbours) > self.k:
+                del array_of_neighbours[array_of_distances.index(max(array_of_distances))]
+                del array_of_distances[array_of_distances.index(max(array_of_distances))]
+            if self.is_weight:
+                for i in range(len(array_of_distances)):
+                    if array_of_distances[i] != 0:
+                        array_of_weight[array_of_neighbours[i]] += 1 / array_of_distances[i]
+                result.append(array_of_weight.index(max(array_of_weight)))
+            else:
+                a, b, c = 0, 0, 0
+                for j in range(len(array_of_neighbours)):
+                    if array_of_neighbours[j] == 0:
+                        a += 1
+                    elif array_of_neighbours[j] == 1:
+                        b += 1
+                    else:
+                        c += 1
+                if max(a, b, c) == a:
+                    result.append(0)
+                elif max(a, b, c) == b:
+                    result.append(1)
                 else:
-                    c += weight - i
-        d = 0
-        if max(a, b, c) == b:
-            d += 1
-        elif max(a, b, c) == c:
-            d += 2
-        dict_test[k_test][1] = d
-    return CV(dict_test)
+                    result.append(2)
+        return result
 
+    def enemy_kNN(self):
+        iris = datasets.load_iris()
+        x = iris.data
+        y = iris.target
+        kNN = KNeighborsClassifier(n_neighbors=3)
+        kNN.fit(x, y)
+        return kNN.score(x, y)
 
-def CV(dict_test):
-    print(dict_test)
-    mistakes = 0
-    n = 0
-    for key, value in dict_test.items():
-        n += 1
-        if dict_test[key][0] != dict_test[key][1]:
+    def cross_validation(self):
+        mistakes = 0
+        result = self.data_processing()
+        if self.unknown_y[0] != result[0]:
             mistakes += 1
-    return (n - mistakes) / n
+        for i in range(1, len(self.unknown_y)):
+            result = self.kNN()
+            if self.unknown_y[i] != result[i]:
+                mistakes += 1
+        return (len(self.unknown_y) - mistakes) / len(self.unknown_y)
 
-
-def grid_search():
-    maximum = 0
-    k = 0
-    for i in range(80):
-        z = data_processing(i)
-        if z > maximum:
-            k = i
-            maximum = z
-    return k
+    def grid_search(self):
+        self.k = 0
+        this_k = self.k
+        min_mistake = self.cross_validation()
+        print("1 /30")
+        for i in range(1, 30):
+            print(i + 1, "/30")
+            self.k = i
+            mis = self.cross_validation()
+            print(mis)
+            if mis < min_mistake:
+                min_mistake = mis
+                this_k = self.k
+        return this_k
 
 
 if __name__ == '__main__':
-    print(data_processing(10))
+    my_kNN = My_kNN()
+    print(my_kNN.enemy_kNN())
+    print(my_kNN.cross_validation())
